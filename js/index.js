82b36372c55be3705c4b9ebd2867498ebd4a3d84
yuan chen <18507040452@163.com>
2018-10-30 10:19:15 +0800

    new

---
+++ js/index.js
@@ -0,0 +1,9 @@
+(function init() {
+    
+    var img = document.getElementsByTagName("img")[0];
+    var imgW = document.documentElement.clientWidth;
+    var imgH = document.documentElement.clientHeight;
+    img.width = imgW;
+    img.height = imgH;
+
+})();
